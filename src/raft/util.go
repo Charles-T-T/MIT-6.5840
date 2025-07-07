089ae44a6e6d07eb6bb18ecb35e06eb68b2352ee
charles_wsl <1592276395@qq.com>
2025-07-07 09:13:36 +0800

    pass Part 3C and 3D

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,13 +1,31 @@
 package raft
 
-import "log"
+import (
+	"log"
+	"os"
+	"strings"
+)
 
 // Debugging
 const Debug = false
+const Log2File = false
+
+var logFile *os.File
+
+func init() {
+	if Debug && Log2File {
+		var err error
+		logFile, err = os.OpenFile("debug.log", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+		if err != nil {
+			log.Fatalf("failed to open log file: %v", err)
+		}
+		log.SetOutput(logFile)
+	}
+}
 
 func DPrintf(format string, a ...interface{}) {
 	if Debug {
-		log.Println("---------------------------------------------------")
+		log.Println(strings.Repeat("-", 100))
 		log.Printf(format, a...)
 	}
 }
